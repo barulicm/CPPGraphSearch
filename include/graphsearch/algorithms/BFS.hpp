5e5ad419f4664c3966d251f89d8e735bd8cbd8bc
Matthew Barulic <matthew.barulic@gmail.com>
2019-09-16 19:04:09 -0700

    Reformats BFS and DFS

--- include/graphsearch/algorithms/BFS.hpp
+++ include/graphsearch/algorithms/BFS.hpp
@@ -10,34 +10,34 @@
 namespace GraphSearch {
 
 /** Runs Breadth-First graph on the given search problem */
-template <class StateType, class ActionType>
-std::optional<Path<StateType, ActionType>> BFS(StateType &startState, std::function<bool(const StateType &)> isGoal,
-                                       std::function<std::vector<ActionType>(const StateType & )> getAllowedActions,
-                                       std::function<StateType(const StateType &, const ActionType &)> getNextState)
-{
+template<class StateType, class ActionType>
+std::optional<Path<StateType, ActionType>>
+BFS(StateType &startState, std::function<bool(const StateType &)> isGoal,
+    std::function<std::vector<ActionType>(const StateType &)> getAllowedActions,
+    std::function<StateType(const StateType &,
+                            const ActionType &)> getNextState) {
   std::set<StateType> expanded;
-  std::queue< Path<StateType, ActionType> > frontier;
+  std::queue<Path<StateType, ActionType> > frontier;
 
-  frontier.push(Path<StateType,ActionType>({startState}, {}));
+  frontier.push(Path<StateType, ActionType>({startState}, {}));
 
-  while(!frontier.empty())
-  {
+  while (!frontier.empty()) {
     auto path = frontier.front();
     frontier.pop();
 
     auto lastState = path.getLastState();
 
-    if( expanded.find(lastState) == expanded.end() ) { // expanded does not contain path's last state
+    if (expanded.find(lastState) == expanded.end()) {
 
       expanded.insert(lastState);
 
-      if(isGoal(lastState)) {
+      if (isGoal(lastState)) {
         return path;
       }
 
       auto allowedActions = getAllowedActions(lastState);
 
-      for( auto &action : allowedActions ) {
+      for (auto &action : allowedActions) {
         auto nextState = getNextState(lastState, action);
 
         Path<StateType, ActionType> newPath(path);
--- include/graphsearch/algorithms/DFS.hpp
+++ include/graphsearch/algorithms/DFS.hpp
@@ -11,32 +11,33 @@ namespace GraphSearch {
 
 /** Runs Depth-First graph search on the given search problem */
 template<class StateType, class ActionType>
-std::optional <Path<StateType, ActionType>>
+std::optional<Path<StateType, ActionType>>
 DFS(StateType &startState, std::function<bool(const StateType &)> isGoal,
-    std::function<std::vector<ActionType>(const StateType & )> getAllowedActions,
-    std::function<StateType(const StateType &, const ActionType &)> getNextState) {
+    std::function<std::vector<ActionType>(const StateType &)> getAllowedActions,
+    std::function<StateType(const StateType &,
+                            const ActionType &)> getNextState) {
 
   std::set<StateType> expanded;
-  std::stack< Path<StateType, ActionType>> frontier;
+  std::stack<Path<StateType, ActionType>> frontier;
 
-  frontier.push(Path<StateType,ActionType>({startState}, {}));
+  frontier.push(Path<StateType, ActionType>({startState}, {}));
 
-  while(!frontier.empty()) {
+  while (!frontier.empty()) {
     auto path = frontier.top();
     frontier.pop();
 
     auto lastState = path.getLastState();
 
-    if( expanded.find(lastState) == expanded.end() ) { // expanded does not contain path's last state
+    if (expanded.find(lastState) == expanded.end()) {
       expanded.insert(lastState);
 
-      if( isGoal(lastState) ) {
+      if (isGoal(lastState)) {
         return path;
       }
 
       auto allowedActions = getAllowedActions(lastState);
 
-      for( auto &action : allowedActions ) {
+      for (auto &action : allowedActions) {
         auto nextState = getNextState(lastState, action);
         Path<StateType, ActionType> newPath(path);
         newPath.addAction(action);
